8f0be13996e598a1b45016272f81852c50705e8c
Nisarat-A <noansrnn@gmail.com>
2023-03-30 17:22:30 +0700

    add parent

--- src/inheritanceFund/Mouse3.java
+++ src/inheritanceFund/Mouse3.java
@@ -2,5 +2,5 @@ package inheritanceFund;
 public class Mouse3 {
     public  static void  lestClick() { System.out.println("click!");}
     public static void rightClick() {System.out.println("click!");}
-
+    boolean ambidextrous = true;
 }
