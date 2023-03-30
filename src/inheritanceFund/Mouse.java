30a38b3214f25c433ea70d1d8a3f753933f03b39
Nisarat-A <noansrnn@gmail.com>
2023-03-30 18:01:34 +0700

    extends generic

--- src/inheritanceFund/Mouse.java
+++ src/inheritanceFund/Mouse.java
@@ -1,7 +1,7 @@
 package inheritanceFund;
 
 public class Mouse {
-    public  static void  lestClick() { System.out.println("click!");}
+    public  static void  leftClick() { System.out.println("click!");}
     public static void rightClick() {System.out.println("click!");}
     public static void ScrollUp() {System.out.println("Scroll Up!");}
     public static void ScrollDown() {System.out.println("Scroll Down!");}
--- src/inheritanceFund/testMouse.java
+++ src/inheritanceFund/testMouse.java
@@ -1,4 +1,12 @@
 package inheritanceFund;
 
 public class testMouse {
+    public static void main(String[] args) {
+        Mouse1 m1 = new Mouse1();
+        m1.leftClick();
+        m1.rightClick();
+        m1.ScrollUp();
+        m1.ScrollDown();
+
+    }
 }
