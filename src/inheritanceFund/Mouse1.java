120d3e1fb4f0262940bc09a45415e7529adcb811
Nisarat-A <noansrnn@gmail.com>
2023-03-30 13:43:43 +0700

    inheritance1.1

--- src/inheritanceFund/Mouse1.java
+++ src/inheritanceFund/Mouse1.java
@@ -1,4 +1,9 @@
 package inheritanceFund;
 public class Mouse1 {
-
+    String texture = "matt" ;
+    public  static void  lestClick() { System.out.println("click!");}
+    public static void rightClick() {System.out.println("click!");}
+    public static void setColor(String color) {
+        System.out.println(color);
+    }
 }
--- src/inheritanceFund/Mouse3.java
+++ src/inheritanceFund/Mouse3.java
@@ -1,4 +1,6 @@
 package inheritanceFund;
 public class Mouse3 {
+    public  static void  lestClick() { System.out.println("click!");}
+    public static void rightClick() {System.out.println("click!");}
 
 }
