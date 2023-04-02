d7f830b891b70083f5523002b73222a44f9fecf1
Nisarat-A <noansrnn@gmail.com>
2023-04-03 00:49:30 +0700

    -

--- src/interface_tue/InterfaceRFun.java
+++ src/interface_tue/InterfaceRFun.java
@@ -2,15 +2,22 @@ package interface_tue;
 interface waterBottle{
     String color = "Blue" ;
     void fillUp();
+    void pourOut();
 }
 public class InterfaceRFun implements waterBottle{
     public static void main(String[] args) {
         System.out.println(color);
         InterfaceRFun i = new InterfaceRFun();
         i.fillUp();
+
     }
     @Override
     public void fillUp() {
         System.out.println("Filled");
+    }
+
+    @Override
+    public void pourOut() {
+        System.out.println();
     }
 }
