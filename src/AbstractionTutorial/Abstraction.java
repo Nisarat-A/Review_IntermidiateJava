74acb91ad67e0f9ef6e8dce60fa740f356dc2e4c
Nisarat-A <noansrnn@gmail.com>
2023-04-03 12:38:53 +0700

    -

--- src/AbstractionTutorial/Abstraction.java
+++ src/AbstractionTutorial/Abstraction.java
@@ -1,3 +1,16 @@
 package AbstractionTutorial ;
+abstract class dog{
+    String breed ;
+    public void bark(){
+        System.out.println("Bark!");
+    }
+        }
+        class shiba extends dog {}
 public class Abstraction {
+    public static void main(String[] args) {
+        shiba s = new shiba();
+        s.bark();
+        s.breed = "shiba inu";
+        System.out.println(s.breed);
+    }
 }
