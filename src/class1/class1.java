9b915207866ebfd5ffb82106b5314fcfcb36d721
Nisarat-A <noansrnn@gmail.com>
2023-04-04 04:54:16 +0700

    -

--- src/class1/class1.java
+++ src/class1/class1.java
@@ -1,4 +1,7 @@
 package class1;
 
 public class class1 {
+    int x =1 ;
+    void sayhi(){System.out.println("Hi");}
+
 }
---
+++ src/class1/main.java
@@ -0,0 +1,9 @@
+package class1;
+
+public class main {
+    public static void main(String[] args) {
+        class1 c = new class1();
+        c.sayhi();
+        System.out.println(c.x);
+    }
+}
